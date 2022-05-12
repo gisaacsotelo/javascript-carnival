bab3f084cea1b191b175d658a9b3927bac8b9742
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-05-12 23:15:43 +1200

    Snippet3 - Add arrays for body n shoes too

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -17,19 +17,18 @@ let currentShoes = document.getElementById('js-head')
 let allHeads = []
 let allBodies = []
 let allShoes = []
-console.log(allHeads)
+
 //-------------- Defaults
 /*
 head defaults:
-Just practicing loops and pushing can be also declared directly will do that with bodies
+Just practicing loops and pushing can be also declared directly, but DRY
 'i' starts in 1 and goes to 5 since there are only 5 images for head -  so goes from 1-5
 */
-
 for (let i = 1; i <= 5; i++) {
   allHeads.push(`./images/head${i}.png`)
-  console.log(allHeads[i - 1], typeof allHeads[i - 1])
+  allBodies.push(`./images/head${i}.png`)
+  allShoes.push(`./images/head${i}.png`)
 }
-console.log(allHeads)
 
 changeClownHead()
 
