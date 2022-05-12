c6f1ea86fae800d5d17b73015cdfd5c3d6ae2a6c
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-05-12 23:12:40 +1200

    Snippet2 - created array of all heads w their strings

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -12,13 +12,24 @@ let currentHead = document.getElementById('js-head')
 let currentBody = document.getElementById('js-head')
 let currentShoes = document.getElementById('js-head')
 
-/* 
-------- variable declarations
-*/
+//------------- Global variable declarations
 
 let allHeads = []
 let allBodies = []
 let allShoes = []
+console.log(allHeads)
+//-------------- Defaults
+/*
+head defaults:
+Just practicing loops and pushing can be also declared directly will do that with bodies
+'i' starts in 1 and goes to 5 since there are only 5 images for head -  so goes from 1-5
+*/
+
+for (let i = 1; i <= 5; i++) {
+  allHeads.push(`./images/head${i}.png`)
+  console.log(allHeads[i - 1], typeof allHeads[i - 1])
+}
+console.log(allHeads)
 
 changeClownHead()
 
