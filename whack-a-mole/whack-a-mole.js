aeea8b934224456c7c302193241a520159af2a80
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-05-14 11:41:49 +1200

    Refactor n reduce comments

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,53 +1,36 @@
 // -    -   -   -   -  //
 // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
-
 console.log('Whack-a-Mole!')
 
-//// - The ability to store or get references to the cells in the HTML table.
-//// - A function to randomly select which cell should show the mole.
-//// - A way to show a mole in the chosen cell.
+//Variables
 
-//get from dom
 let cells = document.getElementsByTagName('td')
-console.log(cells[0].style.width)
-
-//creating an img element for our picture
 let mole = document.createElement('img')
-//setting desired image and height
+let audio = new Audio()
+
+//defaults
 mole.src = 'mole.PNG'
+audio.src = 'whack-audio.wav'
 
 //checkign how tall to make the image:
-/*
-this is just in case they decide to change the cell sizes after, to avoid 
-changing this code tool, otherwise you can just check the css and go with 
-73px or something similar
-*/
-let newHeigth = cells[0].offsetHeight //I think this number includes padding and border and all that
-//to play it safe will just do an 90% of this
+let newHeigth = cells[0].offsetHeight
 newHeigth = Math.floor(newHeigth * 0.9)
 newHeigth = `${newHeigth}px`
 mole.style.width = newHeigth
 
 //get mole in a random cell - initislizing this one get used once only
 cells[Math.floor(Math.random() * 25)].appendChild(mole)
-console.log(mole)
 
 // event listener
 mole.addEventListener('click', changeMole)
 
-// audio
-var audio = new Audio()
-audio.src = 'whack-audio.wav'
-
 //functions
-
 //randomizer between 0-24
 function giveMeARandom() {
   randomNumber = Math.floor(Math.random() * 25)
   return randomNumber
 }
-
 function changeMole() {
   console.log('got clicks')
   audio.play()
